7649eec8a47ec66e4e43acc299229f9449c9083d
JameFLC <jame.flochlecarour@gmail.com>
2022-05-04 16:10:08 +0200

    Added Graphy Performance Monitoring
    A togglable menu attached on left wrist

---
+++ Assets/Scripts/VR/GraphyToggler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Tayx.Graphy;
+
+public class GraphyToggler : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty toggleReference;
+
+    private bool _GraphyEnabled = false;
+    private void Awake()
+    {
+        toggleReference.action.started += ToggleGraphy;
+    }
+    private void OnDestroy()
+    {
+        toggleReference.action.started -= ToggleGraphy;
+    }
+    private void ToggleGraphy(InputAction.CallbackContext context)
+    {
+        GraphyManager.Instance.ToggleActive();
+    }
+    
+}
