94e135735ef534ede93832c98edb15586f50f2d8
JameFLC <jame.flochlecarour@gmail.com>
2022-06-01 09:26:15 +0200

    Autocentering beginning

--- Assets/Scripts/DataIO/Recorders/IOPositionRecorder.cs
+++ Assets/Scripts/DataIO/Recorders/IOPositionRecorder.cs
@@ -19,13 +19,13 @@ public class IOPositionRecorder : IODataRecorder
         if (!_isEnabled)
             return;
 
-        if (Time.time >= _lastTime + updateDelay)
+        if (Time.time >= _lastTime + updateDelay) // Use invoke repeating if performance is an issue
             {
                 _lastTime = Time.time;
                 IODataTypes currentData = new IODataTypes(targetTransform.position);
 
                 data.AddData(currentData);
             }
-
+        
     }
 }
